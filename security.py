cf11b337ab42967b472be8282453928163d0ab12
Tushar Shihab <tushar.shihab13@gmail.com>
2026-05-18 00:22:29 +0600

    chore(security): update login authentication

--- security.py
+++ security.py
@@ -13,6 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 120))
 USERNAME = os.getenv("ADMIN_USERNAME")
+PASSWORD = os.getenv("PASSWORD")
 
 if not SECRET_KEY:
     raise ValueError("SECRET_KEY must be set in environment variables.")
@@ -24,8 +25,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 ADMIN_USER = UserInDB(
-    username="tshihab07",
-    hashed_password="$2b$12$0gUQeg7wcTSHad5HDDzBDebCyIROSomifSXml0PkWF3L6wNf6Uhgm"
+    username=USERNAME,
+    hashed_password=PASSWORD
 )
 
 
